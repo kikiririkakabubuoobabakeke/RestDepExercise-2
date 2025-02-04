53d7f903b00c4d8c3d19c7e26ede4a1195fdb2f4
kikiririkakabubuoobabakeke <s2kt2s123456789@gmail.com>
2025-02-04 12:35:11 +0900

    新規

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/Home")
+	public String Home() {
+		return "Home";
+	}
+
+	@GetMapping("/Learn")
+	public String Learn() {
+		return "Learn";
+	}
+
+	@GetMapping("/Build")
+	public String Build() {
+		return "Build";
+	}
+
+	@GetMapping("/Connect")
+	public String Connect() {
+		return "Connect";
+	}
+
+	@GetMapping("/BLEled")
+	public String blel() {
+		return "BLEled";
+	}
+
+	@GetMapping("/BLEchara")
+	public String blec() {
+		return "BLEchara";
+	}
+
+	@GetMapping("/BLEmotor")
+	public String blem() {
+		return "BLEmotor";
+	}
+
+	@GetMapping("/BLEradio")
+	public String bler() {
+		return "BLEradio";
+	}
+
+	@GetMapping("/Test")
+	public String Test() {
+		return "Test2";
+	}
+
+}
